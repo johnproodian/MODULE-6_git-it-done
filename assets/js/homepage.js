0fc117b3d311336be36afa58ee8f02bdbdd84fa4
Jotas <jproodian721@gmail.com>
2021-12-12 23:41:00 -0600

    formatted the github api url and set up a fetch to the url

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,17 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+
+getUserRepos("johnproodian");
+
+
+// thru 6.1.6; moving breexily thru the next 6.1's, probably onto 6.2
